d265a7ac6a6404275c4193ffd663e7b16d6ebcd3
DAchilles <1005503723@qq.com>
2020-12-11 08:24:42 +0800

    first commit

---
+++ src/Experiment1/testfile.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int a, b;
+    int c=a+b;
+
+    float x, y;
+    float z=x-y;
+
+    return 0;
+}
---
+++ src/Sample/testfile.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int a, b, c;
+    c = a+b;
+    printf("%d", c);
+    return 0;
+}
